527a142b1d37116fffe285c49fa1361db3766022
Yan Rabovik <yan@rabovik.ru>
2013-09-13 12:07:20 +0400

    changed travis test script

--- travis/script.sh
+++ travis/script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-xcodebuild -project RSSwizzleTests/RSSwizzleTests.xcodeproj -scheme RSSwizzleTests-iOS -sdk iphonesimulator -destination OS=7.0,name=iPad -configuration Release test
-xcodebuild -project RSSwizzleTests/RSSwizzleTests.xcodeproj -scheme RSSwizzleTests-iOS -sdk iphonesimulator -destination OS=6.1,name=iPhone -configuration Release test
-xcodebuild -project RSSwizzleTests/RSSwizzleTests.xcodeproj -scheme RSSwizzleTests-iOS -sdk iphonesimulator -destination OS=5.0,name=iPhone -configuration Release test
+#xcodebuild -project RSSwizzleTests/RSSwizzleTests.xcodeproj -scheme RSSwizzleTests-iOS -sdk iphonesimulator -destination OS=7.0,name=iPad -configuration Release test
+#xcodebuild -project RSSwizzleTests/RSSwizzleTests.xcodeproj -scheme RSSwizzleTests-iOS -sdk iphonesimulator -destination OS=6.1,name=iPhone -configuration Release test
+xcodebuild -project RSSwizzleTests/RSSwizzleTests.xcodeproj -scheme RSSwizzleTests-iOS -sdk iphonesimulator5.0 -configuration Release test
 
